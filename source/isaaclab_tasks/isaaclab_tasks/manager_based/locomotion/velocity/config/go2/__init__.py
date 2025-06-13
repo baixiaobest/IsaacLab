042c0237f131fc23eb370ae57087418dbc7b0d8a
Baixiao Huang <baixiaohuang1@gmail.com>
2025-06-13 21:09:11 +0800

    Increase teacher-v3 actor network capacity

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
@@ -154,5 +154,5 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.rough_teacher_env_cfg:UnitreeGo2RoughTeacherCfg_PLAY_v3",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeGo2RoughTeacherPPORunnerCfg_v2"
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeGo2RoughTeacherPPORunnerCfg_v3"
     })
--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/agents/rsl_rl_ppo_cfg.py
@@ -76,6 +76,24 @@ class UnitreeGo2RoughTeacherPPORunnerCfg_v2(RslRlOnPolicyRunnerCfg):
     wandb_project="quadruped"
 
 @configclass
+class UnitreeGo2RoughTeacherPPORunnerCfg_v3(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 1500
+    save_interval = 100
+    experiment_name = "unitree_go2_rough_teacher_v2"
+    empirical_normalization = False
+    policy = RslRlPpoEncoderActorCriticCfg(
+        init_noise_std=1.0,
+        encoder_dims=[397, 256, 128, 64, 32],
+        actor_hidden_dims=[512, 256, 128, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = PPOConfig
+    logger="wandb"
+    wandb_project="quadruped"
+
+@configclass
 class UnitreeGo2RoughTeacherScandotsOnlyPPORunnerCfg(UnitreeGo2RoughTeacherPPORunnerCfg):
     experiment_name = "unitree_go2_rough_teacher_scandots_only"
     policy = RslRlPpoEncoderActorCriticCfg(
