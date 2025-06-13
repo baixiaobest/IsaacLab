a4408281dffce7e1c764bdd201cf8246501f36a1
Baixiao Huang <baixiaohuang1@gmail.com>
2025-06-13 21:20:46 +0800

    update config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
@@ -145,7 +145,7 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.rough_teacher_env_cfg:UnitreeGo2RoughTeacherEnvCfg_v3",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeGo2RoughTeacherPPORunnerCfg_v2"
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeGo2RoughTeacherPPORunnerCfg_v3"
     })
 
 gym.register(
