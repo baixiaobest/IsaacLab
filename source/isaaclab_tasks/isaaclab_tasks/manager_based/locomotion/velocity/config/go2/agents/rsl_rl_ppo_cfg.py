fdb4bb819fd2b91bde578fa7dc072f0d12f7939c
Baixiao Huang <baixiaohuang1@gmail.com>
2025-06-30 13:50:32 +0800

    Fix PPO config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/agents/rsl_rl_ppo_cfg.py
@@ -57,21 +57,6 @@ class UnitreeGo2RoughTeacherPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     logger="wandb"
     wandb_project="quadruped"
 
-NavPPOConfig = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,
-        use_clipped_value_loss=True,
-        clip_param=0.2,
-        entropy_coef=0.005,
-        num_learning_epochs=5,
-        num_mini_batches=4,
-        learning_rate=1.0e-3,
-        schedule="adaptive",
-        gamma=0.99,
-        lam=0.95,
-        desired_kl=0.01,
-        max_grad_norm=1.0,
-    )
-
 @configclass
 class UnitreeGo2RoughTeacherPPORunnerCfg_v2(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
@@ -80,14 +65,13 @@ class UnitreeGo2RoughTeacherPPORunnerCfg_v2(RslRlOnPolicyRunnerCfg):
     experiment_name = "unitree_go2_rough_teacher_v2"
     empirical_normalization = False
     policy = RslRlPpoEncoderActorCriticCfg(
-        init_noise_std=0.8,
-        noise_clip=1.0,
+        init_noise_std=1.0,
         encoder_dims=[397, 256, 128, 64, 32],
         actor_hidden_dims=[512, 256, 128],
         critic_hidden_dims=[512, 256, 128],
         activation="elu",
     )
-    algorithm = NavPPOConfig
+    algorithm = PPOConfig
     logger="wandb"
     wandb_project="quadruped"
 
@@ -131,6 +115,20 @@ class UnitreeGo2FlatPPORunnerCfg(UnitreeGo2RoughPPORunnerCfg):
         self.policy.actor_hidden_dims = [128, 128, 128]
         self.policy.critic_hidden_dims = [128, 128, 128]
 
+NavPPOConfig = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
 
 @configclass
 class UnitreeGo2NavigationPPORunnerCfg_v0(RslRlOnPolicyRunnerCfg):
@@ -140,13 +138,14 @@ class UnitreeGo2NavigationPPORunnerCfg_v0(RslRlOnPolicyRunnerCfg):
     experiment_name = "unitree_go2_navigation_v0"
     empirical_normalization = False
     policy = RslRlPpoEncoderActorCriticCfg(
-        init_noise_std=1.0,
+        init_noise_std=0.8,
+        noise_clip=1.0,
         encoder_dims=[336, 128, 64, 32],
         actor_hidden_dims=[64, 64, 64, 32],
         critic_hidden_dims=[256, 256, 128],
         activation="elu",
         tanh_output=True,
     )
-    algorithm = PPOConfig
+    algorithm = NavPPOConfig
     logger="wandb"
     wandb_project="navigation"
