a4e0e93e5427dbc1cee05d25b70fa9acc5046213
Baixiao Huang <baixiaohuang1@gmail.com>
2025-07-20 11:35:05 +0800

    Change max init terrain level to 10

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/e2e_navigation_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/e2e_navigation_env_cfg.py
@@ -43,7 +43,7 @@ class MySceneCfg(InteractiveSceneCfg):
             prim_path="/World/ground",
             terrain_type="generator",
             terrain_generator=DIVERSE_TERRAINS_CFG,
-            max_init_terrain_level=0,
+            max_init_terrain_level=10,
             collision_group=-1,
             physics_material=sim_utils.RigidBodyMaterialCfg(
                 friction_combine_mode="multiply",
