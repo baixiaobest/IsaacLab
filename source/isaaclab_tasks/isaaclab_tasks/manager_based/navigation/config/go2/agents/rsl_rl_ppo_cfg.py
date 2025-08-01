8146d46d78e1706ab399da9e6f11374da775c86b
Baixiao Huang <baixiaohuang1@gmail.com>
2025-08-01 20:51:54 +0800

    e2e: Collision penalty=-40, reduce model complexity

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/agents/rsl_rl_ppo_cfg.py
@@ -224,8 +224,10 @@ class UnitreeGo2NavigationEnd2EndNoEncoderEnvCfgPPORunnerCfg_v0(RslRlOnPolicyRun
         encoder_obs_normalize=False,
         # actor_hidden_dims=[128, 128, 64, 64],
         # critic_hidden_dims=[128, 128, 64, 64],
-        actor_hidden_dims=[128, 128, 64],
-        critic_hidden_dims=[128, 128, 64],
+        # actor_hidden_dims=[128, 128, 64],
+        # critic_hidden_dims=[128, 128, 64],
+        actor_hidden_dims=[128, 64],
+        critic_hidden_dims=[128, 64],
         activation="elu",
         tanh_output=True,
     )
