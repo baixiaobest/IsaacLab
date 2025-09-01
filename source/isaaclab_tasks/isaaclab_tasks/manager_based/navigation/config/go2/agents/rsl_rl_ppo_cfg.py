371bcbba0f450cef40612e2d584d409e85fa126b
Baixiao Huang <baixiaohuang1@gmail.com>
2025-09-01 11:26:33 +0800

    Add symmetry data augmentation

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/agents/rsl_rl_ppo_cfg.py
@@ -1,6 +1,8 @@
 from isaaclab.utils import configclass
 
-from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg, RslRlPpoEncoderActorCriticCfg
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg, \
+  RslRlPpoEncoderActorCriticCfg, RslRlSymmetryCfg
+import torch
 
 NavPPOConfig = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
@@ -185,6 +187,93 @@ NavE2EPPOConfig = RslRlPpoAlgorithmCfg(
         max_grad_norm=1.0,
     )
 
+def no_encoder_augmentation(
+        env, obs: torch.Tensor, actions: torch.Tensor, obs_type:str="policy") \
+  -> tuple[torch.Tensor, torch.Tensor]:
+    """Data augmentation function that flip robot states and obstacle scan horizontally"""
+    augmented_obs = None
+    augmented_action = None
+
+    def swap_joints_horizontal(joints):
+      joints_flipped = torch.zeros_like(joints, device=joints.device)
+      for i in range(3):
+        joints_flipped[:, 4*i + 0] = joints[:, 4*i + 1]
+        joints_flipped[:, 4*i + 1] = joints[:, 4*i + 0]
+        joints_flipped[:, 4*i + 2] = joints[:, 4*i + 3]
+        joints_flipped[:, 4*i + 3] = joints[:, 4*i + 2]
+      return joints_flipped
+
+    if obs is not None:
+      num_sample, obs_dim = obs.shape
+      augmented_obs = torch.zeros((2*num_sample, obs_dim), device=obs.device, dtype=obs.dtype)
+      augmented_obs[0:num_sample, :] = obs
+      
+      base_lin_vel = obs[:, 0:3]
+      base_ang_vel = obs[:, 3:6]
+      pose_2d_command = obs[:, 6:10]
+      projected_gravity = obs[:, 10:13]
+      joint_pos = obs[:, 13:25]
+      joint_vel = obs[:, 25:37]
+      last_action = obs[:, 37:49]
+      count_down = obs[:, 49:50]
+      obstacle_scan = obs[:, 50: 50+32]
+
+      # Flip horizontally
+      base_lin_vel_flipped = base_lin_vel * torch.tensor([1.0, -1.0, 1.0], device=obs.device)
+      base_ang_vel_flipped = base_ang_vel * torch.tensor([1.0, -1.0, 1.0], device=obs.device)
+      pose_2d_command_flipped = pose_2d_command * torch.tensor([1.0, -1.0, 1.0, -1.0], device=obs.device)
+      projected_gravity_flipped = projected_gravity * torch.tensor([1.0, -1.0, 1.0], device=obs.device)
+      
+      joint_pos_flipped = swap_joints_horizontal(joint_pos)
+
+      joint_vel_flipped = swap_joints_horizontal(joint_vel)
+      
+      last_action_flipped = swap_joints_horizontal(last_action)
+
+      count_down_flipped = count_down
+      obstacle_scan_flipped = torch.flip(obstacle_scan, dims=[1])
+
+      augmented_obs[num_sample:2*num_sample, :] = torch.cat([
+          base_lin_vel_flipped,
+          base_ang_vel_flipped,
+          pose_2d_command_flipped,
+          projected_gravity_flipped,
+          joint_pos_flipped,
+          joint_vel_flipped,
+          last_action_flipped,
+          count_down_flipped,
+          obstacle_scan_flipped
+      ], dim=1)
+
+    if actions is not None:
+      num_sample, action_dim = actions.shape
+      augmented_action = torch.zeros((2*num_sample, action_dim), device=actions.device, dtype=actions.dtype)
+      augmented_action[0:num_sample, :] = actions
+      augmented_action[num_sample:, :] = swap_joints_horizontal(actions)
+
+    return augmented_obs, augmented_action
+
+
+ObstacleScanNoEncoderSymmetryConfig = RslRlSymmetryCfg(
+    use_data_augmentation=True,
+    data_augmentation_func=no_encoder_augmentation)
+
+NavE2EObstacleScanNoEncoderPPOConfig = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.995,
+        lam=0.995,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+        symmetry_cfg=ObstacleScanNoEncoderSymmetryConfig
+    )
+
 @configclass
 class UnitreeGo2NavigationEnd2EndEnvCfgPPORunnerCfg_v0(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
@@ -231,6 +320,6 @@ class UnitreeGo2NavigationEnd2EndNoEncoderEnvCfgPPORunnerCfg_v0(RslRlOnPolicyRun
         activation="elu",
         tanh_output=True,
     )
-    algorithm = NavE2EPPOConfig
+    algorithm = NavE2EObstacleScanNoEncoderPPOConfig
     logger="wandb"
     wandb_project="e2e_navigation"
