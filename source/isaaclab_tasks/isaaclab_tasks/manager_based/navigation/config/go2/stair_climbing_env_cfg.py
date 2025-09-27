4d3abb27354c809aa45d9d0bb19895c1008953d8
Baixiao Huang <baixiaohuang1@gmail.com>
2025-09-27 18:52:13 +0800

    reduce 2nd stage curriculum difficulty

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/stair_climbing_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/stair_climbing_env_cfg.py
@@ -666,10 +666,12 @@ class NavigationEnd2EndStairsOnlyEnvCfg(NavigationStairsEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.scene.terrain.terrain_generator = TURN_90_STAIRS
+        self.scene.terrain.terrain_generator = TURN_90_180_STAIRS
         self.rewards.guidelines_reward.weight = 1.0
         self.rewards.goal_tracking_coarse.weight = 0.0
         self.rewards.undesired_contacts.weight = -20.0
+        self.curriculum.terrain_levels.params['angular_threshold'] = 0.4
+        self.curriculum.terrain_levels.params['distance_threshold'] = 0.8
 
 @configclass
 class NavigationEnd2EndStairsOnlyEnvCfg_PLAY(NavigationEnd2EndStairsOnlyEnvCfg):
