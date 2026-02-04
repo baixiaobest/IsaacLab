54562b8021efd8e9ab74e2193057a497f98a12e4
Baixiao Huang <baixiaohuang1@gmail.com>
2026-02-04 16:38:05 +0800

    guideline rewards=1.0

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/stair_climbing_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/stair_climbing_env_cfg.py
@@ -718,6 +718,7 @@ class NavigationEnd2EndStairsOnlyEnvCfg(NavigationStairsEnvCfg):
         super().__post_init__()
         self.scene.terrain.terrain_generator = TURN_90_STAIRS
         self.rewards.goal_tracking_coarse.weight = 0.0
+        self.rewards.guidelines_reward.weight = 1.0
         self.rewards.undesired_contacts.weight = -2.0
         self.rewards.undesired_contacts.params['threshold'] = 1.0
         self.rewards.movement_reward.params['inactivate_after_time'] = GOAL_REACHED_ACTIVE_AFTER
