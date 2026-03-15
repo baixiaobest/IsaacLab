fe17533f0ea4e21dfa1184594bb813d5f7419c4a
Baixiao Huang <baixiaohuang1@gmail.com>
2026-03-15 23:51:56 +0800

    turn 90 down action rate threshold level=1

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/stair_climbing_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/stair_climbing_env_cfg.py
@@ -751,6 +751,7 @@ class NavigationEnd2EndStairsOnlyEnvCfg(NavigationStairsEnvCfg):
             self.rewards.backward_movement_penalty.weight = -0.5
             self.rewards.backward_movement_penalty.params['heading_deadband'] = 0.1745 # 10 degrees
             self.rewards.action_rate_l2.weight = -0.002 # Avoid jerky actions
+            self.rewards.action_rate_l2.params['terrain_level_threshold'] = 1
 
         # self.curriculum.terrain_levels = CurrTerm(
         #     func=nav_mdp.pose_2d_command_terrain_curriculum_with_threshold, 
