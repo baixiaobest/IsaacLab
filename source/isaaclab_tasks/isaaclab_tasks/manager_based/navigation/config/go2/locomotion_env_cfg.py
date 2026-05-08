47e0b571c10fb3d178ddc3163900d50aba10a6e3
Baixiao Huang <baixiaohuang1@gmail.com>
2026-05-08 22:35:46 +0800

    zero_command_lin_vel_xy_l2=-1.0

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/locomotion_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/locomotion_env_cfg.py
@@ -229,7 +229,7 @@ class RewardsCfg:
     )
     zero_command_lin_vel_xy_l2 = RewTerm(
         func=mdp.zero_command_lin_vel_xy_l2,
-        weight=-3.0,
+        weight=-1.0,
         params={"command_name": "base_velocity", "command_threshold": 0.1},
     )
     zero_command_ang_vel_xy_l2 = RewTerm(
