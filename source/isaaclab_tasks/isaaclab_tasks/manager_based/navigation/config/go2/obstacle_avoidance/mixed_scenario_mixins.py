1005140fdac20932bcf8c15d14ed332331f270b4
Baixiao Huang <baixiaohuang1@gmail.com>
2026-07-08 20:24:37 +0800

    impulse penalty, proximity speed weight=-0.5

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/obstacle_avoidance/mixed_scenario_mixins.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/config/go2/obstacle_avoidance/mixed_scenario_mixins.py
@@ -183,7 +183,7 @@ class _MixedRewardsCfg:
 
     social_force_impulse = RewTerm(
         func=nav_mdp.social_force_impulse,
-        weight=-1.0,
+        weight=-0.5,
         params={
             'sigma': 1.5,
         }
@@ -191,7 +191,7 @@ class _MixedRewardsCfg:
 
     pedestrian_proximity_speed = RewTerm(
         func=nav_mdp.pedestrian_proximity_speed_penalty,
-        weight=-1.0,
+        weight=-0.5,
         params={
             'sigma': 1.5,
         }
